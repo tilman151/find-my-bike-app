4dbab9cb0dd4cfc4e21fa360d40121914a52905d
Tilman Krokotsch <t.krokotsch@gmail.com>
2022-03-07 16:07:50 +0100

    Add pulling port from env variable.

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,13 @@
+import os
+
 import uvicorn
 
 
 if __name__ == "__main__":
-    uvicorn.run("app.api:app", host="0.0.0.0", port=8000, reload=True)
+    if "PORT" in os.environ:
+        port = int(os.environ["PORT"])
+        reload = False
+    else:
+        port = 8000
+        reload = True
+    uvicorn.run("app.api:app", host="0.0.0.0", port=port, reload=reload)
