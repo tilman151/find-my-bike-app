41485e4a0cc558a79abcb117f204b82caec0d68a
Tilman Krokotsch <t.krokotsch@gmail.com>
2022-03-07 20:42:27 +0100

    Reformat files.

--- frontend/src/components/Posting.jsx
+++ frontend/src/components/Posting.jsx
@@ -1,12 +1,13 @@
-import React, { useEffect, useState } from "react";
+import React, {useEffect, useState} from "react";
 import {
-  Select,
-  Stack,
+  Badge,
+  Box,
+  Center,
+  Heading,
   HStack,
   IconButton,
-  Box,
-  Heading,
-  Center, Badge,
+  Select,
+  Stack,
 } from "@chakra-ui/react";
 import {SearchIcon} from "@chakra-ui/icons"
 
@@ -18,15 +19,15 @@ function SearchBar() {
   const [query, setQuery] = React.useState({bike: "", frame: "", color: ""})
   const context = React.useContext(PostingsContext)
 
-  const handleBikeInput = (event)  => {
+  const handleBikeInput = (event) => {
     setQuery({...query, bike: event.target.value})
   }
 
-  const handleFrameInput = (event)  => {
+  const handleFrameInput = (event) => {
     setQuery({...query, frame: event.target.value})
   }
 
-  const handleColorInput = (event)  => {
+  const handleColorInput = (event) => {
     setQuery({...query, color: event.target.value})
   }
 
@@ -36,48 +37,49 @@ function SearchBar() {
   }
 
   return (
-    <form>
-      <HStack pl="0.5rem" pr="0.5rem" pb="0.5rem">
-        <Select
-          variant="flushed"
-          placeholder="Select Bike Type"
-          aria-label="Select Bike Type"
-          onChange={handleBikeInput}
-        >
-          <option value="bike">Bike</option>
-          <option value="children">Children Bike</option>
-          <option value="cargo">Cargo Bike</option>
-        </Select>
-        <Select
-          variant="flushed"
-          placeholder="Select Frame"
-          aria-label="Select Frame"
-          onChange={handleFrameInput}
-        >
-          <option value="diamond">Diamond</option>
-          <option value="trapeze">Trapeze</option>
-          <option value="swan_neck">Swan Neck</option>
-          <option value="low_entry">Low Entry</option>
-          <option value="x">X Frame</option>
-          <option value="y">Y Frame</option>
-        </Select>
-        <Select
-          variant="flushed"
-          placeholder="Select Color"
-          aria-label="Select Color"
-          onChange={handleColorInput}
-        >
-          <option value="black">Black</option>
-          <option value="white">White</option>
-          <option value="gray">Gray</option>
-          <option value="blue">Blue</option>
-          <option value="red">Red</option>
-          <option value="yellow">Yellow</option>
-          <option value="green">Green</option>
-        </Select>
-        <IconButton aria-label='Search Postings' icon={<SearchIcon />} onClick={handleSubmit}/>
-      </HStack>
-    </form>
+      <form>
+        <HStack pl="0.5rem" pr="0.5rem" pb="0.5rem">
+          <Select
+              variant="flushed"
+              placeholder="Select Bike Type"
+              aria-label="Select Bike Type"
+              onChange={handleBikeInput}
+          >
+            <option value="bike">Bike</option>
+            <option value="children">Children Bike</option>
+            <option value="cargo">Cargo Bike</option>
+          </Select>
+          <Select
+              variant="flushed"
+              placeholder="Select Frame"
+              aria-label="Select Frame"
+              onChange={handleFrameInput}
+          >
+            <option value="diamond">Diamond</option>
+            <option value="trapeze">Trapeze</option>
+            <option value="swan_neck">Swan Neck</option>
+            <option value="low_entry">Low Entry</option>
+            <option value="x">X Frame</option>
+            <option value="y">Y Frame</option>
+          </Select>
+          <Select
+              variant="flushed"
+              placeholder="Select Color"
+              aria-label="Select Color"
+              onChange={handleColorInput}
+          >
+            <option value="black">Black</option>
+            <option value="white">White</option>
+            <option value="gray">Gray</option>
+            <option value="blue">Blue</option>
+            <option value="red">Red</option>
+            <option value="yellow">Yellow</option>
+            <option value="green">Green</option>
+          </Select>
+          <IconButton aria-label='Search Postings' icon={<SearchIcon/>}
+                      onClick={handleSubmit}/>
+        </HStack>
+      </form>
   )
 }
 
@@ -92,20 +94,20 @@ const toQueryString = (obj) => {
 
 function Posting(props) {
   return (
-    <Box borderWidth='1px' borderRadius='lg' overflow='hidden' p="0.5rem">
-      <HStack>
-        <Center h="100%" minH="100px" w="100px" bg="tomato">Dummy</Center>
-        <Stack spacing="0.5rem">
-          <Heading size="lg">{props.title}</Heading>
-          <HStack>
-            <Badge>{props.prediction.bike}</Badge>
-            <Badge>{props.prediction.frame}</Badge>
-            <Badge>{props.prediction.color}</Badge>
-          </HStack>
-          <Heading size="s">Berlin</Heading>
-        </Stack>
-      </HStack>
-    </Box>
+      <Box borderWidth='1px' borderRadius='lg' overflow='hidden' p="0.5rem">
+        <HStack>
+          <Center h="100%" minH="100px" w="100px" bg="tomato">Dummy</Center>
+          <Stack spacing="0.5rem">
+            <Heading size="lg">{props.title}</Heading>
+            <HStack>
+              <Badge>{props.prediction.bike}</Badge>
+              <Badge>{props.prediction.frame}</Badge>
+              <Badge>{props.prediction.color}</Badge>
+            </HStack>
+            <Heading size="s">Berlin</Heading>
+          </Stack>
+        </HStack>
+      </Box>
   )
 }
 
@@ -121,13 +123,13 @@ export default function Postings() {
     fetchPostings({bike: "", frame: "", color: ""})
   }, [])
   return (
-    <PostingsContext.Provider value={{postings, fetchPostings}}>
-      <SearchBar/>
-      <Stack spacing={5} pl="0.5rem" pr="0.5rem">
-        {postings.map((posting) => (
-          <Posting title={posting.title} prediction={posting.prediction}/>
-        ))}
-      </Stack>
-    </PostingsContext.Provider>
+      <PostingsContext.Provider value={{postings, fetchPostings}}>
+        <SearchBar/>
+        <Stack spacing={5} pl="0.5rem" pr="0.5rem">
+          {postings.map((posting) => (
+              <Posting title={posting.title} prediction={posting.prediction}/>
+          ))}
+        </Stack>
+      </PostingsContext.Provider>
   )
 }
