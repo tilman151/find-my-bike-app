4f2514d33792db02a76f4c36e2470d2074904426
Tilman Krokotsch <t.krokotsch@gmail.com>
2022-04-06 22:20:23 +0200

    Add validation test.

---
+++ backend/tests/test_validation.py
@@ -0,0 +1,10 @@
+from backend.app import validation
+
+
+def test_flatten():
+    posting = validation.Correction(
+        posting_id=0, correction=validation.Prediction(bike="", frame="", color="")
+    )
+    flattened_posting = validation.flatten(posting, nested="correction")
+    expected = {"posting_id": 0, "bike": "", "frame": "", "color": ""}
+    assert flattened_posting == expected
